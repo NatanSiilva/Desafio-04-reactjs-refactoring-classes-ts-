52536969809af40e07abc002679bfebe8b9f6731
Natanael Silva <natanael.younner@gmail.comm>
2021-03-28 16:47:36 -0300

    Projeto finalizado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,38 @@
-import { Component } from 'react';
 import { FiPlusSquare } from 'react-icons/fi';
 
 import { Container } from './styles';
 import Logo from '../../assets/logo.svg';
+
+interface HeaderProps {
+  openModal:() => void;
+}
+
+export default function Header({openModal}: HeaderProps){
+
+  return (
+    <Container>
+      <header>
+        <img src={Logo} alt="GoRestaurant" />
+        <nav>
+          <div>
+            <button
+              type="button"
+              onClick={openModal}
+            >
+              <div className="text">Novo Prato</div>
+              <div className="icon">
+                <FiPlusSquare size={24} />
+              </div>
+            </button>
+          </div>
+        </nav>
+      </header>
+    </Container>
+  )
+}
+
+
+/*
 
 class Header extends Component {
   render() {
@@ -32,3 +62,5 @@ class Header extends Component {
 };
 
 export default Header;
+
+*/
